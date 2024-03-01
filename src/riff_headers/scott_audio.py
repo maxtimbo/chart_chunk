0ce8d629caa492e9b65632ef1255ebd5a92e281c
Tim Finley <tfinley@pop-os.localdomain>
2024-02-29 22:14:34 -0500

    completed scott chunk interpreter

--- src/riff_headers/scott_audio.py
+++ src/riff_headers/scott_audio.py
@@ -5,34 +5,53 @@ from pathlib import Path
 from wavdata.wavdata import WavData
 
 
-@click.command()
+@click.group()
 @click.argument('audio', type=click.Path(exists=True))
-@click.option('--create_copy', is_flag=True)
-def cli(audio, create_copy):
+@click.pass_context
+def cli(ctx, audio):
     audio = Path(audio)
     if audio.suffix == '.wav':
-        wav_info = WavData(audio)
-        wav_info.get_riff_data()
+        wav = WavData(audio)
+        wav.get_riff_data()
         click.echo(f'{"RIFF DATA":^40}')
-        for k, v in wav_info.riff_data.items():
+        for k, v in wav.riff_data.items():
             click.echo(f'{k:<20}: {v}')
 
-        click.echo(wav_info.current_position)
 
         click.echo(f'\n{"DATA META":^40}')
-        for k, v in wav_info.data_meta.items():
+        for k, v in wav.data_meta.items():
             click.echo(f'{k:<20}: {v}')
 
-        if wav_info.is_scott:
+        if wav.is_scott:
             click.echo(f'\n{"The file has a scott header":^40}')
             click.echo(f'{"SCOT DATA":^40}')
-            for k, v in wav_info.scott_data.items():
+            for k, v in wav.scott_data.items():
                 click.echo(f'{k:<20}: {v}')
+
         else:
             click.echo(f'\n{"The file does not have a scott header":^40}')
 
+        click.echo(f'End of riff/fmt chunk: {wav.riff_fmt_end}')
+        if wav.is_scott:
+            click.echo(f'Scott begin byte position: {wav.scott_begin}')
+            click.echo(f'Scott end byte position: {wav.scott_end}')
+            click.echo(f'Scott end fillout byte position: {wav.scott_end_fillout}')
+
+        click.echo(f'Data byte position: {wav.data_position}')
+        ctx.ensure_object(dict)
+        ctx.obj['wav'] = wav
+
     else:
         click.echo('File must be a wav')
+
+@cli.command()
+@click.argument('new_audio')
+@click.argument('artist')
+@click.argument('title')
+@click.pass_context
+def create_copy(ctx, new_audio, artist, title):
+    wav = ctx.obj.get('wav')
+    #wav.create_copy(new_audio, artist, title)
 
 
 if __name__ == '__main__':
--- src/riff_headers/wavdata/wavdata.py
+++ src/riff_headers/wavdata/wavdata.py
@@ -8,11 +8,18 @@ from datetime import datetime, timedelta
 class WavData:
     def __init__(self, filename: pathlib.Path) -> None:
         self.filename = filename
-        self.current_position: int
-        self.riff_data: dict = {}
-        self.data_meta: dict = {}
-        self.is_scott: bool = False
-        self.scott_data: dict = {}
+
+        self.riff_fmt_end: int
+        self.data_position: int
+        self.scott_begin: int
+        self.scott_end: int
+        self.scott_end_fillout: int
+
+        self.riff_data: dict    = {}
+        self.data_meta: dict    = {}
+        self.scott_data: dict   = {}
+
+        self.is_scott: bool     = False
 
     def get_riff_data(self):
         with io.open(self.filename, 'rb') as fh:
@@ -28,24 +35,41 @@ class WavData:
             self.riff_data['transfrate']   = riff_data[8]
             self.riff_data['align']        = riff_data[9]
             self.riff_data['bitspersamp']  = riff_data[10]
-            self.current_position = fh.tell()
+            self.riff_fmt_end = fh.tell()
             search_bytes = fh.read(550)
             self.get_data_size(search_bytes)
             self.get_scott_data(search_bytes)
+
+    def get_mpeg_data(self):
+        with io.open(self.filename, 'rb') as fh:
+            fh.seek(self.riff_fmt_end)
+            mpeg = struct.unpack('<2hl4h2l', fh.read(24))
+            self.riff_data['extra']     = mpeg[0]
+            self.riff_data['layer']     = mpeg[1]
+            self.riff_data['bitrate']   = mpeg[2]
+            self.riff_data['mode']      = mpeg[3]
+            self.riff_data['extmode']   = mpeg[4]
+            self.riff_data['emphasis']  = mpeg[5]
+            self.riff_data['flags']     = mpeg[6]
+            self.riff_data['PTSlow']    = mpeg[7]
+            self.riff_data['PTShigh']   = mpeg[8]
 
     def get_scott_data(self, search_bytes):
         index = search_bytes.find(b'scot')
         if index != -1:
             self.is_scott = True
+            self.scott_begin = index + self.riff_fmt_end
+            if self.scott_begin - self.riff_fmt_end == 24:
+                self.get_mpeg_data()
             with io.open(self.filename, 'rb') as fh:
-                fh.seek(index + self.current_position)
+                fh.seek(self.scott_begin)
                 scott_data = struct.unpack(
-                    '<4slB43p3p4sc5shhhh6s6sbbchcBlHL12sLLlhh3s4sc3s4sc21s108s34s34s2sc4scb6sHHBLHLLL45s',
-                    fh.read(426)
+                    '<4slB43p3p4sc5shhhh6s6sbbchcBlHL12sLLlhh3s4sc3s4sc21s108s34s34s2sc4scb6sHHBLH3L',
+                    fh.read(381)
                 )
                 self.scott_data['scott']        = scott_data[0].decode('ascii')
                 self.scott_data['cksize']       = scott_data[1]
-                self.scott_data['attrib']       = scott_data[2]
+                self.scott_data['attrib']       = bin(scott_data[2])
                 self.scott_data['title']        = scott_data[3].decode('utf-8', 'ignore')
                 self.scott_data['unknown1']     = scott_data[4]
                 self.scott_data['cart']         = scott_data[5].decode('ascii')
@@ -100,11 +124,19 @@ class WavData:
                 self.scott_data['chopsize']     = scott_data[50]
                 self.scott_data['vteomovr']     = scott_data[51]
                 self.scott_data['desiredlen']   = scott_data[52]
-                self.scott_data['trigger1']     = scott_data[53]
-                #self.scott_data['trigger2']     = scott_data[54]
-                #self.scott_data['trigger3']     = scott_data[55]
-                #self.scott_data['trigger4']     = scott_data[56]
-                #self.scott_data['fillout']      = scott_data[57]
+                self.scott_end = fh.tell()
+                scraps = struct.unpack('<14s3s16s18s', fh.read(51))
+                self.scott_data['fillout1']     = scraps[0]
+                self.scott_data['category']     = scraps[1]
+                self.scott_data['fillout2']     = scraps[2]
+                self.scott_data['fillout3']     = scraps[3]
+                self.scott_end_fillout = fh.tell()
+                till_data = self.data_position - self.scott_end_fillout
+                if till_data == 12:
+                    fact_chunk = struct.unpack(f'<4sll', fh.read(till_data))
+                    self.scott_data['fact_chunk']   = fact_chunk[0]
+                    self.scott_data['fact_size']    = fact_chunk[1]
+                    self.scott_data['numsamples']   = fact_chunk[2]
 
         else:
             self.is_scott = False
@@ -113,8 +145,9 @@ class WavData:
         index = search_bytes.find(b'data')
         try:
             if index != -1:
+                self.data_position = index + self.riff_fmt_end
                 with io.open(self.filename, 'rb') as fh:
-                    fh.seek(index + self.current_position)
+                    fh.seek(self.data_position)
                     data, audio_length = struct.unpack('<4sl', fh.read(8))
                     self.data_meta['tag'] = data.decode('ascii')
                     self.data_meta['length'] = audio_length
@@ -122,12 +155,17 @@ class WavData:
         except:
             raise
 
+    #def write_artist_title(self, new_file: pathlib.Path, artist: str, title: str):
+    #    riff_chunk = struct.pack()
+    #    data_chunk = struck.pack()
+
+    #def compile_scott(self, artist: str, title: str):
+    #    scott_chunk = struck.pack(
+    #        '<4slB43p3p4sc5shhhh6s6sbbchcBlHL12sLLlhh3s4sc3s4sc21s108s34s34s2sc4scb6sHHBLH3L',
+
+
     @staticmethod
     def convert_timestamp(date, hour_value):
-        #if hour_value >= 128:
-        #    hour = hour_value - 128
-        #else:
-        #    hour = hour_value
         hour = hour_value + 128
         hours = f'{hour:02d}{0:02d}{0:02d}'
         date = date.decode('ascii')
